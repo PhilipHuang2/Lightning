fce47dfd5be31041108c2354f8845338ccb59691
unknown <PHHUANG@697-334-Gandalf.sf.k12.internal>
2014-09-02 10:30:00 -0700

    some syntax changes

--- Lightning.pde
+++ Lightning.pde
@@ -2,21 +2,24 @@ int startX = 0;
 int startY = 300;
 int endX = 5;
 int endY = 300;
+
 void setup()
 {
   size(600,600);
   background(0);
+  stroke(12,125,200);
+  
   
 }
+
 void draw(){
-  stroke(12,125,200);
   line(startX,startY,endX,startY);
+  endX = endX + (int)((Math.random()*10)+1);
+  endY = endY + (int)((Math.random()*10)-5);
   startX = endX;
   startY = endY;
-  endX = endX + (int)(Math.random()*10+1);
-  endY = endY + (int)(Math.random()*10-5);
-  
-  
+
+  // problem: for each line startY = end Y
 }
 
 void mousePressed()
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,35 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX = 0;
+int startY = 300;
+int endX = 5;
+int endY = 300;
+
 public void setup()
 {
-  size(300,300);
-  noLoop();
+  size(600,600);
+  background(0);
+  stroke(12,125,200);
+  
+  
 }
-public void draw()
-{
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+
+public void draw(){
+  line(startX,startY,endX,startY);
+  endX = endX + (int)((Math.random()*10)+1);
+  endY = endY + (int)((Math.random()*10)-5);
+  startX = endX;
+  startY = endY;
+
+  // problem: for each line startY = end Y
 }
+
 public void mousePressed()
 {
-  redraw();
+   size(600,600);
+   background(0);
+
 }
 
   static public void main(String[] passedArgs) {
